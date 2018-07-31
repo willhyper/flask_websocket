1e20a399078f18994c2e1451cde903a8aa3b1664
willhyper <willhyper@gmail.com>
2018-07-31 02:37:21 -0700

    change my event to from_frontend_to_backend

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'secret!'
+# app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 from functools import partial
+
 from_backend_to_frontend = partial(socketio.emit, 'from_backend_to_frontend', namespace='/test')
+
 
 def background_stuff():
     """ Let's do it a bit cleaner """
@@ -23,23 +25,23 @@ def index():
     return render_template('index.html')
 
 
-@socketio.on('my event', namespace='/test')
+@socketio.on('from_frontend_to_backend', namespace='/test')
 def my_event(msg):
-    print(msg['data'])
+    print(msg)
 
 
 @socketio.on('connect', namespace='/test')
 def on_connect():
     # emit('my response', {'data': 'Connected', 'count': 0})
-    # socketio.emit('from_backend_to_frontend', , namespace='/test')
     from_backend_to_frontend({'message': 'connected'})
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected')
 
 
-
 if __name__ == '__main__':
     Thread(target=background_stuff, daemon=True).start()
     socketio.run(app, host='0.0.0.0', port=8888, debug=True)
+
